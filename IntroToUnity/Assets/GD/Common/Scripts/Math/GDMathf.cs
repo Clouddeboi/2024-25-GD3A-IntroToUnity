a752cfb8fe8dbe24c8d386148ae653259f30995e
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-24 11:08:43 +0100

    :tada: Initial commit

---
+++ IntroToUnity/Assets/GD/Common/Scripts/Math/GDMathf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace GD
+{
+    public static class GDMathf
+    {
+        /// <summary>
+        /// Clamps an angle in degrees to the range [-360, 360]
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < -360f) angle += 360f;
+            if (angle > 360f) angle -= 360f;
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        /// <summary>
+        /// Converts degrees to radians
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        internal static float ToRadians(float degrees)
+        {
+            //degrees %= 360f;
+            //180 degrees = Mathf.PI
+            return Mathf.PI * degrees / 180f;
+        }
+    }
+}
