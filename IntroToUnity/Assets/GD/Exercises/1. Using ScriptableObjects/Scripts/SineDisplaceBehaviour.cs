4c7893d08fda39407a208510617015b45d6be751
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-25 14:24:45 +0100

    Update SineDisplaceBehaviour.cs

--- IntroToUnity/Assets/GD/Exercises/1. Using ScriptableObjects/Scripts/SineDisplaceBehaviour.cs
+++ IntroToUnity/Assets/GD/Exercises/1. Using ScriptableObjects/Scripts/SineDisplaceBehaviour.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 /// </summary>
 public class SineDisplaceBehaviour : MonoBehaviour
 {
+    //A*Sin(wT + phi)
+    //A: Amplitude, w: angular speed, phi: phase shift
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float amplitude = 1;
+
+    [SerializeField]
+    private Vector3 direction = Vector3.up;
+
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = target.transform.position;
+    }
+
+    private void Update()
+    {
+        var displacement = amplitude * Mathf.Sin(Time.time);
+        target.transform.position
+            = originalPosition + direction * displacement;
+    }
 }
