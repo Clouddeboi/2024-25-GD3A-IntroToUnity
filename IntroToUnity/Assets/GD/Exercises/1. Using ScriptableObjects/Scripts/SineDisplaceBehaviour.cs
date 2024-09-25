f41da45be503cfc2b85f2f48459a3bb7ab2661d0
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-25 13:21:51 +0100

    :truck: Added Exercise 1 on sinusoidal movement

---
+++ IntroToUnity/Assets/GD/Exercises/1. Using ScriptableObjects/Scripts/SineDisplaceBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+/// <summary>
+/// Applies a sinusoidal displacement on a game object along a user-defined direction, amplitude, and frequency
+/// </summary>
+public class SineDisplaceBehaviour : MonoBehaviour
+{
+}
