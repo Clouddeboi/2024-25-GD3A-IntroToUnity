cc22daeed40c20c0ed65d269248fc3866bb8bb2f
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-09-26 08:33:40 +0100

    :construction: Formatting

--- IntroToUnity/Assets/GD/Exercises/1. Using ScriptableObjects/Scripts/SineDisplaceBehaviour.cs
+++ IntroToUnity/Assets/GD/Exercises/1. Using ScriptableObjects/Scripts/SineDisplaceBehaviour.cs
@@ -13,10 +13,21 @@ public class SineDisplaceBehaviour : MonoBehaviour
     [SerializeField]
     private float amplitude = 1;
 
+    //TODO - ALL - Replace float with FloatParameter, FloatVariable, FloatReference
+
     [SerializeField]
     private Vector3 direction = Vector3.up;
 
+    [SerializeField]
+    [Range(0, 10)]
+    private float freqMultiplier = 1;
+
+    [SerializeField]
+    [Range(-180, 180)]
+    private float phaseAngleDegrees = 0;
+
     private Vector3 originalPosition;
+    private float elapsedTimeSecs;
 
     private void Start()
     {
@@ -25,8 +36,17 @@ public class SineDisplaceBehaviour : MonoBehaviour
 
     private void Update()
     {
-        var displacement = amplitude * Mathf.Sin(Time.time);
+        elapsedTimeSecs += Time.deltaTime;
+        var displacement = amplitude
+            * Mathf.Sin(freqMultiplier * elapsedTimeSecs
+            + ToRadians(phaseAngleDegrees));
         target.transform.position
             = originalPosition + direction * displacement;
+    }
+
+    private float ToRadians(float degrees)
+    {
+        //180 degs = Mathf.PI
+        return Mathf.PI * degrees / 180f;
     }
 }
